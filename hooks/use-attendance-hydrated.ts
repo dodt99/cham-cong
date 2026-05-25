939db74559f57d02d2dbaf131f07d7e581a04c38
dodt <tiendo220199@gmail.com>
2026-05-25 16:37:51 +0700

    update

--- hooks/use-attendance-hydrated.ts
+++ hooks/use-attendance-hydrated.ts
@@ -5,15 +5,19 @@ import { useEffect, useState } from "react";
 import { useAttendanceZustandStore } from "@/stores/attendance-store";
 
 export function useAttendanceHydrated() {
-  const [hydrated, setHydrated] = useState(
-    () => useAttendanceZustandStore.persist.hasHydrated(),
-  );
+  const [hydrated, setHydrated] = useState(false);
 
   useEffect(() => {
-    const unsub = useAttendanceZustandStore.persist.onFinishHydration(() => {
-      setHydrated(true);
-    });
+    const markHydrated = () => setHydrated(true);
+
+    if (useAttendanceZustandStore.persist.hasHydrated()) {
+      markHydrated();
+      return;
+    }
+
+    const unsub = useAttendanceZustandStore.persist.onFinishHydration(markHydrated);
     void useAttendanceZustandStore.persist.rehydrate();
+
     return unsub;
   }, []);
 
